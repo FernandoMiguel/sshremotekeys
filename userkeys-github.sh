910449778eba7fcbb2fc318d46103171dd5c9175
Fernando Miguel <FernandoMiguel@users.noreply.github.com>
2017-04-15 15:23:42 +0100

    Update userkeys-github.sh
    Temporarily change URL to avoid accidentally grant access

--- userkeys-github.sh
+++ userkeys-github.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-curl -sf https://github.com/USER.keys
+curl -sf https://github.com/$$USER.keys
+#TODO test and add variable
+
 # ideally all keys are in ed25519 format, to make them much smaller compared to RSA2048 
 # for example, see https://github.com/fernandomiguel.keys
