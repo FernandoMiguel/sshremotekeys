20ef6bb0224adc1832ca36cb57ccad4926d56f0a
Fernando Miguel <FernandoMiguel@users.noreply.github.com>
2017-04-15 13:46:42 +0100

    add comments

--- sshauth-install.sh
+++ sshauth-install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+# download to the host your custom userkeys.sh
 wget https://s3.amazonaws.com/BUCKET/userkeys.sh -O /usr/local/bin/userkeys.sh
+
+# set file permissions
 chmod 555 /usr/local/bin/userkeys.sh
 
+# modifies sshd_config with the following settings:
+# enables publickey login
 echo "AuthenticationMethods publickey" >> /etc/ssh/sshd_config
+# configures AuthorizedKeysCommand to execute userkeys.sh on each login
 echo "AuthorizedKeysCommand /usr/local/bin/userkeys.sh" >> /etc/ssh/sshd_config 
+# sets the user to nobody to avoid escalation privilege
 echo "AuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config 
 
+# restart ssh or sshd depending of the distro
 service ssh || sshd restart
