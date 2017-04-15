0bd22affd688cf08c1821c233bb55f5a33bc23e5
Fernando Miguel <FernandoMiguel@users.noreply.github.com>
2017-04-15 21:26:49 +0100

    Update userkeys-github.sh

--- userkeys-github.sh
+++ userkeys-github.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# add your own github users here and uncomment
+# add your own github user(s) here and uncomment
 # curl -sf https://github.com/$$USER.keys
-#TODO test and add variable
 
 # ideally all keys are in ed25519 format, to make them much smaller compared to RSA2048 
 # for example, see https://github.com/fernandomiguel.keys
