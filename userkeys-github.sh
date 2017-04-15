d3541adccbc8fa3d4d7d42a70b1db226f1746b11
Fernando Miguel <FernandoMiguel@users.noreply.github.com>
2017-04-15 11:14:45 +0100

    add example

--- userkeys-github.sh
+++ userkeys-github.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -sf https://github.com/USER.keys
 # ideally all keys are in ed25519 format, to make them much smaller compared to RSA2048 
+# for example, see https://github.com/fernandomiguel.keys
