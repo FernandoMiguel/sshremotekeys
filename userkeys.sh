dc50771a3bf3b276433d8c088be23ee44468f6c4
Fernando Miguel <FernandoMiguel@users.noreply.github.com>
2017-04-18 11:22:49 +0100

    commented both URLs and added further comments to clear user own repos

--- userkeys.sh
+++ userkeys.sh
@@ -3,7 +3,10 @@
 set -e
 
 # uri="https://s3.amazonaws.com/BUCKET/devs.keys"
-uri="https://github.com/""$USER"".keys"
+# uri="https://github.com/""$USER"".keys"
+# Choose one of the above examples or your own key repository. 
+# if using the exmaples, make sure you point to your own URL or you might accidently grant access to someone else!
+
 # ideally all keys are in ed25519 format, to make them much smaller compared to RSA2048
 
 cacheFile="$HOME""/.ssh/ak_cache"
