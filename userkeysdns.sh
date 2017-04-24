a8471da91e72f0577bf164a60e54a84e6a74145a
Fernando Miguel <FernandoMiguel@users.noreply.github.com>
2017-04-24 16:27:12 +0100

    merge userhome

--- userkeysdns.sh
+++ userkeysdns.sh
@@ -10,7 +10,13 @@ set -e
 # ideally all keys are in ed25519 format, to make them much smaller compared to RSA2048
 # to generate a ed25519 key, run $ ssh-keygen -t ed25519
 
-cacheFile="$HOME""/.ssh/authorized_keys_cache"
+# get's the user trying to authenticate $home path
+userHome=$(getent passwd "$1" | cut -d":" -f6)
+
+# location of the key cache file
+cacheFile="$userHome""/.ssh/authorized_keys_cache"
+
+# how long do we accept the cached keys before pulling again
 cacheValidity="300"
 
 if [ ! -f "$cacheFile" ] || [ "$(($(date +%s) - $(stat -c %Y "$cacheFile")))" -gt "$cacheValidity" ]
