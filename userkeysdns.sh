96be285d97ea73783fb920fc1cc29e78437ad3d0
Fernando Miguel <FernandoMiguel@users.noreply.github.com>
2017-05-02 16:51:54 +0100

    Update userkeysdns.sh

--- userkeysdns.sh
+++ userkeysdns.sh
@@ -5,7 +5,7 @@ set -e
 #############################################################################
 # Modify and uncomment the example bellow or your own DNS record. 
 #############################################################################
-# uri="ssh.DOMAIN.TLD"
+uri="ssh.fernandomiguel.net"
 # if using the examples, make sure you point to your own URL or you might accidently grant access to someone else!
 # ideally all keys are in ed25519 format, to make them much smaller compared to RSA2048
 # to generate a ed25519 key, run $ ssh-keygen -t ed25519
