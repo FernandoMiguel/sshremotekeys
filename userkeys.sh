86fa979517925c53efc79e9f7dbbcad839974eb4
Fernando Miguel <FernandoMiguel@users.noreply.github.com>
2017-11-13 15:45:51 +0000

    Update userkeys.sh

--- userkeys.sh
+++ userkeys.sh
@@ -6,7 +6,7 @@ set -e
 # Modify and uncomment one of the examples bellow or your own key repository. 
 #############################################################################
 # uri="https://s3.amazonaws.com/BUCKET/devs.keys"
-# uri="https://github.com/""$USER"".keys"
+uri="https://github.com/fernandomiguel.keys"
 # if using the examples, make sure you point to your own URL or you might accidently grant access to someone else!
 # ideally all keys are in ed25519 format, to make them much smaller compared to RSA2048
 # to generate a ed25519 key, run $ ssh-keygen -t ed25519
