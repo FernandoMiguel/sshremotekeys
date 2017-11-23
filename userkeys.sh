1f5a3e8e6f8967e87f5c0d99ddbb1e1a641c5a75
Fernando Miguel <FernandoMiguel@users.noreply.github.com>
2017-11-23 14:26:47 +0000

    harcode user path

--- userkeys.sh
+++ userkeys.sh
@@ -5,17 +5,13 @@ set -e
 #############################################################################
 # Modify and uncomment one of the examples bellow or your own key repository. 
 #############################################################################
-# uri="https://s3.amazonaws.com/BUCKET/devs.keys"
 uri="https://github.com/fernandomiguel.keys"
 # if using the examples, make sure you point to your own URL or you might accidently grant access to someone else!
 # ideally all keys are in ed25519 format, to make them much smaller compared to RSA2048
 # to generate a ed25519 key, run $ ssh-keygen -t ed25519
 
-# get's the user trying to authenticate $home path
-userHome=$(getent passwd "$1" | cut -d":" -f6)
-
 # location of the key cache file
-cacheFile="$userHome""/.ssh/authorized_keys_cache"
+cacheFile=/root/.ssh/authorized_keys_cache"
 
 # how long do we accept the cached keys before pulling again
 cacheValidity="300"
